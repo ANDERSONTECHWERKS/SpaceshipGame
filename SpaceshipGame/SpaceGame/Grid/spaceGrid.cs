5ef049e9ff0975a3b69ae7ad2352b6dcaa414980
ANDERSONTECHWERKS <20546666+ANDERSONTECHWERKS@users.noreply.github.com>
2020-01-11 17:30:17 -0500

    Added comments and TODO's

--- SpaceshipGame/SpaceGame/Grid/spaceGrid.cs
+++ SpaceshipGame/SpaceGame/Grid/spaceGrid.cs
@@ -6,58 +6,65 @@ namespace SpaceshipGame.Grid
 {
     public class spaceGrid
     {
-        //Creates a 2d grid of true/false where false represents asteroids and true represents open space.
-        //A second 2d grid of spaceship objects exist alongside. This one will be populated with ship objects and nulls (For empty space)
+        //Creates a 2d spacegrid of true/false where false represents asteroids and true represents open space.
+        //TODO: Someday, turn the spacegrid into an array of ints that can represent multiple hazards (Black holes, wormholes, etc.)
+        private bool[,] spacegrid = new bool[10,10];
 
-            //TODO: Someday, turn the grid into an array of ints that can represent multiple hazards (Black holes, wormholes, etc.)
+        //Establish named constants for use in 'spacegrid'
+        public const bool ASTEROID = false;
+        public const bool SPACE = true;
 
-        private bool[,] grid = new bool[10,10];
 
-
+        //A second 2d spacegrid of spaceship objects exist alongside. This one is populated with ship objects and nulls (For empty space)
         private shipClassInterface[,] shipGrid = new shipClassInterface[10,10];
         
 
-        //Default grid: 10x10 empty grid with asteroids bordering the sides.
+        //Default spacegrid: 10x10 empty spacegrid with asteroids bordering the sides.
         public spaceGrid()
         {
             for (int i = 0; i < 10; i++)
             {
                 for (int j = 0; j < 10; j++)
                 {
-                    grid[i , j] = true;
+                    spacegrid[i , j] = true;
                 }
             }
             for (int i = 0; i < 10; i++)
             {
-                grid[i,0] = false;
-                grid[0,i] = false;
-                grid[9,i] = false;
-                grid[i,9] = false;
+                spacegrid[i,0] = false;
+                spacegrid[0,i] = false;
+                spacegrid[9,i] = false;
+                spacegrid[i,9] = false;
                 
             }
         }
 
+        //isAsteroid: Checks for Asteroid (Boolean: False) at the row / col specified.
         public Boolean isAsteroid(int row,int col)
         {
-            if (grid[row,col] == false)
+            if (spacegrid[row,col] == false)
             {
                 return true;
             }
             return false;
         }
 
+        //addAsteroid: Adds asteroid at the row / col specified
+        //TODO: Add check for whether or not an asteroid already exists, as well as a ship.
         public void addAsteroid(int row, int col)
         {
-            this.grid[row,col] = false;
+            this.spacegrid[row,col] = false;
         }
         public void removeAsteroid(int row, int col)
         {
-            this.grid[row,col] = true;
+            this.spacegrid[row,col] = true;
         }
 
+        //AddShip: adds a ship object at the selected row / col in the ShipGrid.
+        //TODO: Check for existing ship or asteroid at row / col.
         public void AddShip(shipClassInterface ship, int row, int col)
         {
-            if (grid[row,col] == false && (shipGrid[row,col] == null))
+            if (spacegrid[row,col] == false && (shipGrid[row,col] == null))
             {
                 shipGrid[row, col] = ship;
                 ship.locationRow = row;
@@ -65,9 +72,12 @@ namespace SpaceshipGame.Grid
             }
 
         }
+
+        //RemoveShip: sets the specified row / col to null, effectively "Removing" the ship.
+        //TODO: Create a check that ensures we are actually removing a ship.
         public void RemoveShip(int row, int col)
         {
-            if (grid[row, col] == true && (shipGrid[row, col] != null))
+            if (spacegrid[row, col] == true && (shipGrid[row, col] != null))
             {
                 shipGrid[row, col] = null;
             }
--- SpaceshipGame/SpaceGame/Runtime/SpaceGame.cs
+++ SpaceshipGame/SpaceGame/Runtime/SpaceGame.cs
@@ -1,5 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using SpaceshipGame;
+using SpaceshipGame.Components;
+using SpaceshipGame.Grid;
+using SpaceshipGame.ShipAssembler;
 namespace SpaceshipGame
 {
     class SpaceGame
@@ -10,8 +14,11 @@ namespace SpaceshipGame
             Console.WriteLine("---------- G-A-M-E ---------------------------");
             Console.WriteLine("------------------ -- 8 8 --------------------");
 
+            //gameGrid: Instantiate a new, default 10x10 spacegrid
+            spaceGrid gameGrid = new spaceGrid();
+
             //assembledShip: Runs the user through the ship creation menu and assigns the current
-            ShipAssembler.AssembledShip assembledShip = ShipAssembler.AssembledShip.AssembleMenu();
+            AssembledShip assembledShip = AssembledShip.AssembleMenu();
 
             assembledShip.ToString();
         }
